1f262c55bc0fce9a034b783d058c22051d359265
ArshilKhan09 <arshilkhan529@gmail.com>
2026-07-27 18:45:29 +0530

    Initial Commit

---
+++ light.py
@@ -0,0 +1,15 @@
+#traffic light code
+
+light = input("light color : ")
+
+if(light == "Red") :
+    print("Stop ")
+
+elif(light == "Yellow,") :
+    print("Start")
+
+elif(light == "Green,") :
+    print("Go")
+
+else :
+    print("Light is Broken")
