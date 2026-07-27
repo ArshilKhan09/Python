5c0a7405964b635ff1e510b184c08cdcfa2449eb
ArshilKhan09 <arshilkhan529@gmail.com>
2026-07-27 21:07:05 +0530

    Added new var1.py for understanding variable & datatype

---
+++ variable/var1.py
@@ -0,0 +1,32 @@
+#int 
+a = 54378
+
+#float
+b = 7.57
+
+#boolean
+c = True
+
+#string
+d = "Hello, World!"
+
+#list
+e = [1, 2, 3, 4, 5]
+
+#tuple
+f = (1, 2, 3)
+
+#dictionary
+g = {"name": "Alice", "age": 30, "city": "New York"}
+
+#set
+h = {1, 2, 3, 4, 5}
+
+#None data type
+i = None
+
+#complex
+j = complex(2, 3)
+
+#bytes
+k = bytes("Hello", "utf-8")
--- words.py
+++
@@ -1,7 +0,0 @@
-r = lambda q : q*2
-s = lambda q : q*3
-
-x = 2 
-x = r(x) 
-x = s(x)
-print(x) 
