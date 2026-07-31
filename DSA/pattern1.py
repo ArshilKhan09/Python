3145c686bd5be966a368e8112f4bd617243ba97c
ArshilKhan09 <arshilkhan529@gmail.com>
2026-07-31 21:28:32 +0530

    Optimized code by adding input of row and column

--- DSA/pattern1.py
+++ DSA/pattern1.py
@@ -1,8 +1,8 @@
-n = int(input("Enter the number : "))
+row = int(input("Enter the number of rows: "))
+column = int(input("Enter the number of columns: "))
 
-
-for outer in range(n):
-    for inner in range(n):
+for outer in range(row):
+    for inner in range(column):
         print("*", end=" ")
     
     print()
